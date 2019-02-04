971e725f2b957843d5cdd39131df6f54e2e26dd3
sanjithvenugopal <47182880+sanjithvenugopal@users.noreply.github.com>
2019-02-04 20:53:30 +0530

    Update charvowelconso.py

--- charvowelconso.py
+++ charvowelconso.py
@@ -1,7 +1,10 @@
 #sanjith
 x=input()
-y=x.lower()
-if y=='a' or y=='e' or y=='i' or y=='o' or y=='u':
-	print("Vowel")
+if x.isalpha():
+	y=x.lower()
+	if y=='a' or y=='e' or y=='i' or y=='o' or y=='u':
+		print("Vowel")
+	else:
+		print("Consonant")
 else:
-	print("Consonant")
+	print("invalid")
